e64ae7d4d56fe8159106a7c6ebb436bdf27915e1
badreddinelyazgi <Elyazgi98@gmail.com>
2026-02-20 01:58:28 -0500

    Adding CRUD testing workflow

--- jdbc_crud/src/main/java/org/horrorcore/Main.java
+++ jdbc_crud/src/main/java/org/horrorcore/Main.java
@@ -5,6 +5,7 @@ import org.horrorcore.entity.Sub;
 import org.horrorcore.repository.SubRepository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     static void main() {
@@ -16,6 +17,7 @@ public class Main {
 
         Sub sub = new Sub("Dreadnought", "Virginia-class", 4300000000.00);
         SubRepository subRepository = new SubRepository();
+        long id = sub.getSubId();
 
         subRepository.save(sub);
 
@@ -24,5 +26,41 @@ public class Main {
                         .concat(sub.getSubName())
                         .concat(", Sub Id: ")
                         .concat( String.valueOf(sub.getSubId())));
+
+        System.out.println("----------------------------------------------");
+        List<Sub> allSubs = subRepository.findAll();
+        System.out.println("Total Subs: " + allSubs.size());
+        for (Sub subAll : allSubs) {
+            System.out.println(subAll.getSubId() + " " + subAll.getSubName() + " " + subAll.getSubType() + " " + subAll.getCost());
+        }
+
+        System.out.println(" ------------------------------------------------");
+
+        Sub foundSub = subRepository.findById(sub.getSubId());
+        if (foundSub != null) {
+            System.out.println("Found Sub: " + foundSub.getSubName() + " " + foundSub.getSubType());
+        } else {
+            System.out.println("No sub found with id: " + sub.getSubId());
+        }
+
+        System.out.println("---------------------------------------------------");
+
+        sub.setSubName("Dreadnought UPDATE");
+        sub.setCost(4400000000.00);
+        subRepository.update(sub);
+
+        Sub afterUpdateSub = subRepository.findById(sub.getSubId());
+        System.out.println("After Update Sub: " + afterUpdateSub.getSubName() + " " + afterUpdateSub.getSubType());
+
+
+        System.out.println("-----------------------------------------------------");
+
+        subRepository.delete(sub.getSubId());
+        System.out.println("Deleted Sub: " + sub.getSubId());
+
+
     }
+
+
+
 }
